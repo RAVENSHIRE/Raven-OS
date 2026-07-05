22366531e9f73f1586dfee1e97f35bd0e153a737
Ravenshire Labs GmbH <83118376+RAVENSHIRE@users.noreply.github.com>
2026-07-05 11:37:21 +0000

    Fix BootScreen parser error and localize desktop icons

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -1,14 +1,13 @@
 import DesktopIcon from "./DesktopIcon";
 import { useOS } from "../context/OSContext";
 import type { WindowId } from "../context/OSContext";
-
-const windowsExplorerIcon = new URL("../../node_modules/@react95/icons/png/WindowsExplorer_32x32_4.png", import.meta.url).href;
-const joyIcon = new URL("../../node_modules/@react95/icons/png/Joy102_32x32_4.png", import.meta.url).href;
-const globeIcon = new URL("../../node_modules/@react95/icons/png/Globe_32x32_4.png", import.meta.url).href;
-const notepadIcon = new URL("../../node_modules/@react95/icons/png/Notepad_32x32_4.png", import.meta.url).href;
-const infoIcon = new URL("../../node_modules/@react95/icons/png/InfoBubble_32x32_4.png", import.meta.url).href;
-const floppyIcon = new URL("../../node_modules/@react95/icons/png/ReaderDisket_32x32_4.png", import.meta.url).href;
-const gameIcon = new URL("../../node_modules/@react95/icons/png/Freecell1_32x32_4.png", import.meta.url).href;
+import windowsExplorerIcon from "../assets/icons/WindowsExplorer_32x32_4.png";
+import joyIcon from "../assets/icons/Joy102_32x32_4.png";
+import globeIcon from "../assets/icons/Globe_32x32_4.png";
+import notepadIcon from "../assets/icons/Notepad_32x32_4.png";
+import infoIcon from "../assets/icons/InfoBubble_32x32_4.png";
+import floppyIcon from "../assets/icons/ReaderDisket_32x32_4.png";
+import gameIcon from "../assets/icons/Winmine1_32x32_4.png";
 
 type IconDef = {
   id: WindowId;
